938fe499e0c2ab5fa39cfd7c49e111f1e2e88114
Mudassir Kidwai <kidwaimudassir@gmail.com>
2020-06-25 23:21:04 +0500

    Updates

---
+++ precache-manifest.7e0d4b4e6bfff9642af7ffe0e267dfd7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee7ab11950162d1d5be8c2d28c396154",
+    "url": "/RoboApp/index.html"
+  },
+  {
+    "revision": "eabd1a8b852b8b505d0b",
+    "url": "/RoboApp/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "0ef902a6d7fd5b1ec285",
+    "url": "/RoboApp/static/css/main.5b7ed7eb.chunk.css"
+  },
+  {
+    "revision": "eabd1a8b852b8b505d0b",
+    "url": "/RoboApp/static/js/2.a5747d58.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/RoboApp/static/js/2.a5747d58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ef902a6d7fd5b1ec285",
+    "url": "/RoboApp/static/js/main.ed144f07.chunk.js"
+  },
+  {
+    "revision": "eafc9a71ea85bcba73e7",
+    "url": "/RoboApp/static/js/runtime-main.f0a3ba10.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboApp/static/media/SEGA.12a3db8b.woff"
+  }
+]);
